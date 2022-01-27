0de0c0c46abbec12cac68a22d63337f617ea7cb5
manixcoder <pathakmanish86@gmail.com>
2022-01-27 18:26:55 +0530

    listing code

--- app/Http/Controllers/Admin/PermissionsManagementController.php
+++ app/Http/Controllers/Admin/PermissionsManagementController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller; 
 use App\Models\Permission;
 use App\Models\PermissionRoleRelation;
 use App\Models\Role;
